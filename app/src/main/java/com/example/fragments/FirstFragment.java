5a42135067d29914dd715aa98fb65e5b50b21ade
Aryan1800 <buddhadas1800@gmail.com>
2021-11-03 13:37:05 +0530

    fragmentsv2

--- app/src/main/java/com/example/fragments/FirstFragment.java
+++ app/src/main/java/com/example/fragments/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.fragments.R;
 
@@ -20,12 +22,24 @@ public class FirstFragment extends Fragment {
     }
 
 
+    TextView tv;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
        View view = inflater.inflate(R.layout.fragment_first, container, false);
 
+       tv = view.findViewById(R.id.textView);
+
+       tv.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getContext() , SecondActivity.class);
+               startActivity(intent);
+           }
+       });
 
 
 
